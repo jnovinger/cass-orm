84ee2a5588438d8bf40768e1c776ff88db45d438
lusana <krone@jupiter.(none)>
2013-06-22 10:45:24 +1000

    removed hard coded cfs

--- cass/utils/unittest.py
+++ cass/utils/unittest.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.conf import settings
-from django.test.utils import override_settings
 from cass.models.db import Cassandra
-from pycassa import ConnectionPool, ColumnFamily
 
 
 class CassTestCase(TestCase):
@@ -12,9 +10,5 @@ class CassTestCase(TestCase):
         if hasattr(settings, 'TRUNCATE_CFS'):
             for cf in settings.TRUNCATE_CFS:
                 Cassandra.Instance().cf(cf).truncate()
-
-        Cassandra.Instance().cf('users').truncate()
-        Cassandra.Instance().cf('user_emails').truncate()
-        Cassandra.Instance().cf('user_friends').truncate()
 
         super(CassTestCase, self).setUp()
